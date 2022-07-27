92e7f5761930006a9e4414116bf64878a3697928
GlaurungSH <chiachenkosergey@gmail.com>
2022-07-27 15:15:23 +0300

    Homework 2 lesson

---
+++ data_types.py
@@ -0,0 +1,33 @@
+number_1 = 4
+number_2 = 12
+number_3 = 6
+number_4 = 192
+number_5 = 34
+line_1 = 'Hello'
+line_2 = 'My name is Sergey'
+line_3 = 'I am 26 years old'
+line_4 = 'I live in UA'
+line_5 = 'I like Python'
+
+
+print("number_1 =", number_1)
+print(number_1, type(number_1))
+print("number_2 =", number_2)
+print(number_2, type(number_2))
+print("number_3 =", number_3)
+print(number_3, type(number_3))
+print("number_4 =", number_4)
+print(number_4, type(number_4))
+print("number_5 =", number_5)
+print(number_5, type(number_5))
+
+print("Say Hello -", line_1)
+print(line_1, type(line_1))
+print("What is your name? -", line_2)
+print(line_2, type(line_2))
+print("How old are you? -", line_3)
+print(line_3, type(line_3))
+print("Where are you from? -", line_4)
+print(line_4, type(line_4))
+print("What do you like? -", line_5)
+print(line_5, type(line_5))
