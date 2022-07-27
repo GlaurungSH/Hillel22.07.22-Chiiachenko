944ee3e24b71668dcfe7c2b46011dbc334d8858e
GlaurungSH <chiachenkosergey@gmail.com>
2022-07-27 21:03:17 +0300

    Homework 2 lesson task 2

---
+++ rows_and_numbers.py
@@ -0,0 +1,23 @@
+user_name = input('Write your name: ')
+result = f"Hi {user_name}, welcome"
+print(result)
+
+user_number = input('Please write your number: ')
+entered_number = float(user_number)
+print('Your number: ', entered_number)
+
+int_number = int(entered_number)
+print('Your integer number: ', int_number)
+
+number_power = entered_number ** 4
+print('Your number to the fourth power: ', number_power)
+
+root_int_number = int_number ** 0.5
+print('The square root of your integer number: ', root_int_number)
+
+modulo = int_number % 2
+print('Remainder of dividing an integer by two: ', modulo)
+
+
+
+
