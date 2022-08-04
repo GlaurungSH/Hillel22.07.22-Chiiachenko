eeee1faf4f60aaddc8f53133a55f0f0c9ff63827
GlaurungSH <chiachenkosergey@gmail.com>
2022-08-04 21:38:55 +0300

    Homework lesson 4 calculator

---
+++ flow_control.py
@@ -0,0 +1,51 @@
+print('Hello. I am a simple calculator')
+
+number_1 = input('Please enter the first number -> ')
+if "." in number_1:
+    number_1 = float(number_1)
+else:
+    number_1 = int(number_1)
+
+operator = input('Please enter an action for the calculator -> ')
+
+number_2 = input('Please enter the second number -> ')
+if "." in number_2:
+    number_2 = float(number_2)
+else:
+    number_2 = int(number_2)
+
+
+if operator == "+":
+        print(f"{number_1} + {number_2} =", number_1 + number_2)
+
+elif operator == "-":
+    print(f"{number_1} - {number_2} =", number_1 - number_2)
+
+elif operator == "*":
+    print(f"{number_1} * {number_2} =", number_1 * number_2)
+
+elif operator == "/":
+    try:
+        result = number_1 / number_2
+        if number_1 % number_2 != 0:
+            result = float(result)
+        else:
+            result = int(result)
+        print(f"{number_1} / {number_2} =", result)
+    except ZeroDivisionError:
+        print("You can't dividing on zero")
+
+elif operator == "**":
+    print(f"{number_1} ** {number_2} =", number_1 ** number_2)
+
+else:
+    print('You have entered an invalid operation character. Restart the program')
+
+
+
+
+
+
+
+
+
