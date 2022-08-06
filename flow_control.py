62f9f813387f2b30cf64c7b369658f4964298ab6
GlaurungSH <chiachenkosergey@gmail.com>
2022-08-06 21:45:30 +0300

    Homework lesson 4 calculator

--- flow_control.py
+++ flow_control.py
@@ -1,45 +1,47 @@
-print('Hello. I am a simple calculator')
+import sys
 
+print('Hello. I am a simple calculator')
 number_1 = input('Please enter the first number -> ')
-number_1 = float(number_1) if "." in number_1 else int(number_1)
+try:
+    number_1 = float(number_1) if "." in number_1 else int(number_1)
+except ValueError:
+    print("You cannot use letters. Please use numbers")
+    sys.exit(0)
 
 number_2 = input('Please enter the second number -> ')
-number_2 = float(number_2) if "." in number_2 else int(number_2)
+try:
+    number_2 = float(number_2) if "." in number_2 else int(number_2)
+except ValueError:
+    print("You cannot use letters. Please use numbers")
+    sys.exit(0)
 
 operator = input('Please enter an action for the calculator -> ')
 
 if operator == "+":
-    sum_numbers = number_1 + number_2
-    print(f"{number_1} + {number_2} =", sum_numbers)
+    result = number_1 + number_2
 
 elif operator == "-":
-    number_subtraction = number_1 - number_2
-    print(f"{number_1} - {number_2} =", number_subtraction)
+    result = number_1 - number_2
 
 elif operator == "*":
-    multiplic_numbers = number_1 * number_2
-    print(f"{number_1} * {number_2} =", multiplic_numbers)
+    result = number_1 * number_2
 
 elif operator == "/":
     try:
         result = number_1 / number_2
         result = float(result) if number_1 % number_2 != 0 else int(result)
-        print(f"{number_1} / {number_2} =", result)
     except ZeroDivisionError:
         print("You can't dividing on zero")
+        sys.exit(0)
 
 elif operator == "**":
-    power_number = number_1 ** number_2
-    print(f"{number_1} ** {number_2} =", power_number)
+    result = number_1 ** number_2
 
 else:
     print('You have entered an invalid operation character. Restart the program')
+    sys.exit(0)
 
-
-
-
-
-
+print("Result ->", result)
 
 
 
