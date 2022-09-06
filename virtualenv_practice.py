40b9898c85b0c35f90b7eaac2a3f0781dc6f5984
GlaurungSH <chiachenkosergey@gmail.com>
2022-09-06 14:46:21 +0300

    Homework lesson 11

---
+++ virtualenv_practice.py
@@ -0,0 +1,17 @@
+"""Task_1"""
+
+# 1. Create and activate a virtual environment
+# command in terminal -> sudo pip3 install virtualenv
+# command in terminal -> virtualenv -p python3.10 /Users/sergeychiiachenko/Desktop/Hillel/venv/hillel_basic
+# command in terminal -> source /Users/sergeychiiachenko/Desktop/Hillel/venv/hillel_basic/bin/activate
+
+# 2. Install several modules of any kind
+# command in terminal -> pip install requests
+# command in terminal -> pip install kivy
+# command in terminal -> pip install python-telegram-bot
+
+# 3. Save the list of project dependencies in requirements.txt
+# command in terminal -> pip freeze > requirements.txt
+
+# 4. Deactivate the virtual environment
+# command in terminal -> deactivate
