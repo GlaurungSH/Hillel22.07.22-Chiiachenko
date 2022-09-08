177b895e185eaa8ce8baca95e1d8adcf1b3706b1
GlaurungSH <chiachenkosergey@gmail.com>
2022-09-08 15:58:18 +0300

    Homework lesson 12

--- schedules/files_worker.py
+++ schedules/files_worker.py
@@ -26,11 +26,11 @@ with open('schedule.csv', 'w') as csv_table:
 
 # Create a function that reads the content of schedule.csv and returns the entire content of the file
 # as a dictionary or list
-dict_empty = []
+list_empty = []
 with open('schedule.csv', 'r') as schedule:
     reader = csv.DictReader(schedule)
     for row in reader:
-        dict_empty.append(row)
+        list_empty.append(row)
         print(row)
 
 
@@ -39,4 +39,4 @@ with open('schedule.csv', 'r') as schedule:
 # 3.1) The keys in the recorded file must be sorted.
 # 3.2) Attachments within the file must be indented with 4 spaces.
 with open('schedule.json', 'w', newline='') as fh:
-    json.dump(dict_empty, fh, indent=4, sort_keys=True)
+    json.dump(list_empty, fh, indent=4, sort_keys=True)
